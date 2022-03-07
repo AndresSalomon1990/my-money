61173e31b69e0760d1412aa6b31fd8dec0d9986a
AndresSalomon1990 <a.salomon.1990@gmail.com>
2022-03-06 21:13:14 -0300

    Setup auth state change observer in authContext and use it in App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
+import { useAuthContext } from './hooks/useAuthContext';
 
 // pages & components
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Signup from './pages/signup/Signup'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
+  const { authIsReady } = useAuthContext();
+
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/signup'>
-            <Signup />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { createContext, useReducer } from 'react';
+import { projectAuth } from '../firebase/config';
 
 export const AuthContext = createContext();
 
@@ -8,13 +10,28 @@ export const authReducer = (state, action) => {
       return { ...state, user: action.payload };
     case 'LOGOUT':
       return { ...state, user: null };
+    case 'AUTH_IS_READY':
+      return { ...state, user: action.payload, authIsReady: true };
     default:
       return state;
   }
 };
 
 export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, { user: null });
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    authIsReady: false,
+  });
+
+  // to check if the user is logged in or not
+  useEffect(() => {
+    const unsub = projectAuth.onAuthStateChanged((user) => {
+      dispatch({ type: 'AUTH_IS_READY', payload: user });
+
+      // unsuscribe the observer once the auth service is ready
+      unsub();
+    });
+  }, []);
 
   console.log('AuthContext state:', state);
 
