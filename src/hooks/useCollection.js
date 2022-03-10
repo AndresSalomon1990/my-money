4f44e425b84cca836c17800f0dc4ccfca9325669
AndresSalomon1990 <a.salomon.1990@gmail.com>
2022-03-10 00:39:59 -0300

    Create query and orderBy functionality in useCollection to order and grab data for the logged user

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,12 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { projectFirestore } from '../firebase/config';
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query, _orderBy) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
+  // as _query is an array (a reference type), it will trigger a re-evaluation of the component
+  // everytime it changes as it will be "different" every function call, triggering an infinite loop in useEffect
+  // so wrapping it in useRef it will be always the same
+  const query = useRef(_query).current;
+  const orderBy = useRef(_orderBy).current;
+
   useEffect(() => {
     let ref = projectFirestore.collection(collection);
+
+    if (query) {
+      // if there are query parameters in the hook, update the ref with that query by spreading the array
+      // this is used to only get the docs related to an user
+      ref = ref.where(...query);
+    }
+
+    if (orderBy) {
+      ref = ref.orderBy(...orderBy);
+    }
 
     const unsubscribe = ref.onSnapshot(
       (snapshot) => {
@@ -28,7 +44,7 @@ export const useCollection = (collection) => {
 
     // unsubscribe on unmount
     return () => unsubscribe();
-  }, [collection]);
+  }, [collection, query, orderBy]);
 
   return { documents, error };
 };
